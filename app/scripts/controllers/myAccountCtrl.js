dd7dc01f89c0a8e8736aaf3239a96f0b4244ae96
pallav dubey <pallavdubey@gmail.com>
2016-01-20 22:47:53 +0530

    changed acconding to reviews (ng-repeat in myAccountCtrl.js)

--- app/scripts/controllers/myAccountCtrl.js
+++ app/scripts/controllers/myAccountCtrl.js
@@ -12,4 +12,12 @@ angular.module('testAppApp')
 
     $scope.userData =  mainService.username;
     $scope.showMessage = 'WELCOME';
+
+    $scope.details = [
+  {name:'Salomi', number:1234},
+  {name:'pallav', number:5678},
+  {name:'babul', number:9012},
+  {name:'rana', number:345},
+  {name:'me d bestest:)', number:14},
+	]
   });
